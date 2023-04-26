9a63998a5006bf8d674c0a25581d08a62e6cc2b7
Cristian Álvarez <94159947+CristianAlvarezMartinez@users.noreply.github.com>
2023-04-26 15:09:39 -0500

    m-06-Fixed (#11)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,21 +10,21 @@ export default function Navbar() {
   setHide(!hide)
 }
   return (
-    <nav className='flex justify-between align-center px-4 mt-4 h-14 | lg:border-b-2 lg:border-white/25 lg:h-24 lg:justify-end | md:h-16 md:px-8 md:mt-8 | 2xl:h-28 2xl:px-16 2xl:'>
+    <nav className='flex justify-between align-center px-4 mt-4 h-14 | lg:border-b-2 lg:border-white/25 lg:h-24 | md:h-16 md:px-8 md:mt-8 | 2xl:h-28 2xl:px-16 2xl:'>
         <div className={`fixed bg-gradient-to-r from-[#434343] to-[#000] top-0 right-0 bottom-0 left-0 z-20 py-8 px-5 ${hide ? 'hidden' : ''} `}>
           <div className='flex justify-between'>
           <BiUserCircle className='text-white text-5xl'/>
           <VscChromeClose onClick={show} className="text-white text-2xl my-auto"/>
           </div>
           <div className='flex flex-col gap-5 text-center text-white mt-8 font-bold'>
-          <a href="#" className='py-3 active:bg-white active:text-black rounded-lg'>Home</a>
-          <a href="#" className='py-3 active:bg-white active:text-black rounded-lg'>My mangas</a>
-          <a href="#" className='py-3 active:bg-white active:text-black rounded-lg'>Mangas</a>
-          <a href="#" className='py-3 active:bg-white active:text-black rounded-lg'>Favourites</a>
-          <a href="#" className='py-3 active:bg-white active:text-black rounded-lg'>Logout</a>
+            <a href="#" className='py-3 active:bg-white active:text-black rounded-lg'>Home</a>
+            <a href="#" className='py-3 active:bg-white active:text-black rounded-lg'>My mangas</a>
+            <a href="#" className='py-3 active:bg-white active:text-black rounded-lg'>Mangas</a>
+            <a href="#" className='py-3 active:bg-white active:text-black rounded-lg'>Favourites</a>
+            <a href="#" className='py-3 active:bg-white active:text-black rounded-lg'>Logout</a>
           </div>
         </div>    
-        <AiOutlineMenu className='text-white text-3xl | md:block md:text-3xl | lg:hidden | 2xl:hidden' onClick={show}/>
+        <AiOutlineMenu className='text-white text-3xl | md:block md:text-3xl | lg:mt-4 | xl:text-4xl | 2xl:text-5xl' onClick={show}/>
         <img src={logo} alt="Logo imagen" className='w-24 my-auto | md:w-28 | lg:mb-7 | 2xl:w-40 2xl:h-20' />
     </nav>
   )
